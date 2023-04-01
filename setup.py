e044dd0eb00498ae87aad98bb2a5fa677bf20a40
Douglas Nunes <dgspai@hotmail.com>
2023-04-01 18:12:54 -0300

    feat: Create initial code skeleton

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+__version__ = "0.0.0"
+
+setup(
+    name='dataops-mlops',
+    version=__version__,
+    description='Basic project to show an ML application using DevOps, DataOps and MlOps concepts.',
+    url='https://github.com/nunes-douglas-ai/dataops-mlops',
+    author='Douglas Nunes',
+    author_email='nunes.douglas.ai@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python :: 3.9',
+    ]
+)
