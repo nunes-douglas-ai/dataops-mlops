e174ffce0d2f66b0b3733fea3baafceb34ade5da
Douglas Nunes <dgspai@hotmail.com>
2023-04-01 18:12:54 -0300

    feat: Create initial code skeleton

---
+++ web-app/app/main.py
@@ -0,0 +1,15 @@
+import uvicorn
+
+from configs.app_configs import AppSettings
+
+if __name__ == "__main__":
+    app_settings = AppSettings()
+
+    uvicorn.run(
+        "app:app",
+        host="0.0.0.0",
+        port=app_settings.port,
+        reload=True,
+        log_level=app_settings.log_level,
+        workers=app_settings.workers
+    )
