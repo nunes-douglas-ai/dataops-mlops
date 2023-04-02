b51a31c7abe018b29351d8ed75ddb7b2fed734cd
nunes-douglas-ai <129620637+nunes-douglas-ai@users.noreply.github.com>
2023-04-02 13:14:00 -0300

    feat: Add first unit test

--- web-app/app/controllers/hello_controller.py
+++ web-app/app/controllers/hello_controller.py
@@ -1,10 +1,13 @@
 from fastapi import APIRouter
 
-hello_router = APIRouter()
+from entities.hello_entities import HelloResponse, HelloRequest
+from services.hello_service import HelloService
+
+hello_router = APIRouter(prefix="/hello", tags=["hello"])
+
+service = HelloService()
 
 
-@hello_router.get("/talk")
-async def talk():
-    return {
-        "message": "Olá mundo!"
-    }
+@hello_router.post("/talk")
+async def talk(request: HelloRequest) -> HelloResponse:
+    return service.process_talk(request)
